710f5d74824dde326085f981e1fc6888a03492cf
Shreya <shreyarnaik03@gmail.com>
2025-01-24 18:07:58 +0530

    Updated index.html

--- app.py
+++ app.py
@@ -4,26 +4,6 @@ import json
 
 app = Flask(__name__)
 
-# # Flashcards data
-# decks = {
-#     "Group1": [
-#         {
-#             "word": "Equivocate",
-#             "info": "<b>Part of Speech:</b> Verb<br><b>Definition:</b> To use ambiguous language to conceal the truth or avoid committing to a position.<br><b>Example:</b> The politician equivocated when asked about his stance on the issue."
-#         },
-#         {
-#             "word": "Loquacious",
-#             "info": "<b>Part of Speech:</b> Adjective<br><b>Definition:</b> Tending to talk a great deal; talkative.<br><b>Example:</b> She was so loquacious that meetings lasted twice as long as necessary."
-#         }
-#     ],
-#     "Group2": [
-#         {
-#             "word": "Obfuscate",
-#             "info": "<b>Part of Speech:</b> Verb<br><b>Definition:</b> To render obscure, unclear, or unintelligible.<br><b>Example:</b> The professor's lengthy explanation only served to obfuscate the main point."
-#         }
-#     ]
-# }
-
 # Load deck data from the JSON file
 with open("deck_data.json", "r") as file:
     decks = json.load(file)
@@ -32,16 +12,6 @@ with open("deck_data.json", "r") as file:
 def index():
     return render_template("index.html", decks=decks)
 
-# @app.route("/get_card", methods=["POST"])
-# def get_card():
-#     data = request.json
-#     deck_name = data.get("deck")
-#     card_index = data.get("index", 0)
-
-#     if deck_name in decks and 0 <= card_index < len(decks[deck_name]):
-#         card = decks[deck_name][card_index]
-#         return jsonify({"success": True, "card": card})
-#     return jsonify({"success": False, "message": "Invalid deck or card index"})
     
 @app.route("/group/<group_name>")
 def group(group_name):
